45ecde988187654b2df3d3044ad6578232a07de3
“Hege <hege@checkin.no>
2023-09-26 10:29:07 +0200

    GirlTechNew

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,29 @@ import "./App.css";
 import { Lotto } from "./components/Lotto";
 
 function App() {
-    return (
-        <div className="AppHeader">
-            <div className="App">
-                <p>
-                    Checkin sin
-                    <br></br>
-                    GirlTech lotto-trekning!
-                </p>
-                <p className="mindreTekst">
-                    Lag deg 3 rekker med 7 tall i hver (fra 1 - 21)
-                    <br></br> - Sett ring rundt tallene som trekkes ut
-                </p>
-                {/*     <img
-                    className="rabbitPhoto"
-                    src={"/lotto" + require("./components/photos/kanin.jpg")}
-                    alt="rabbit"
-                /> */}
-                <Lotto />
-            </div>
-        </div>
-    );
+  return (
+    <div className="AppHeader">
+      <div className="App">
+        <p>
+          Checkin sin
+          <br></br>
+          GirlTech lotto-trekning!
+        </p>
+        <p className="mindreTekst">
+          Lag deg 2 rekker med 7 tall i hver (fra 1 - 21)
+          <br></br> - Sett ring rundt tallene som trekkes ut,
+          <br></br> og x på tilleggstallet.
+          <br></br>
+          <br></br> - Trykk på "nytt tall" for å få et nytt tall.
+          <br></br> - Etter 7 tall, trykk på "tilleggstall" for å få et
+          tilleggstall.
+          <br></br> - Trykk på tilleggstallet for å sortere tallene.
+          <br></br> - Trykk på -X- for å fjerne alt og starte på ny.
+        </p>
+        <Lotto />
+      </div>
+    </div>
+  );
 }
 
 export default App;
